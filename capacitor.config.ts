cae0632a4c9961008d0ed65882de659fd8773d36
AyoubBeliych <beliychayoub@gmail.com>
2025-07-28 22:06:21 +0100

    Prepare Android build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.horoscope',
+  appName: 'Horoscope App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
